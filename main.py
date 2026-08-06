e5b6b515ed2336303b9a6e2eb0c2984ac0c0482f
ssuparare <saharatssr5@gmail.com>
2026-08-06 20:58:22 +0700

    organized a bit

--- main.py
+++ main.py
@@ -9,16 +9,22 @@ def example2d2class():
 
     print(f"\nLinear SVM with 2D data, 2 class\n")
 
-    # class 0: (1,2), (2,3)
-    # class 1: (7,9), (8,5)
+    # class 0: [65,47],[61,37],[47,46],[50,36],[62,27]
+    # class 1: [86,25],[66,25],[91,27],[87,28],[81,28]
 
     x = numpy.array([
-        [1,2],
-        [2,3],
-        [7,9],
-        [8,5],
+        [65,47],
+        [61,37],
+        [47,46],
+        [50,36],
+        [62,27],
+        [86,25],
+        [66,25],
+        [91,27],
+        [87,28],
+        [81,28],
     ], dtype = float)
-    y = numpy.array([0, 0, 1, 1])
+    y = numpy.array([0, 0, 0, 0, 0, 1, 1, 1, 1, 1])
 
     model = LinearSVM(learningRate = 1e-3, lambdaParam = 1e-2, iteration = 1000)
     model.fit(x, y)
@@ -33,7 +39,7 @@ def example2d2class():
 
     fig, ax = plt.subplots(figsize = (6, 6))
 
-    for cls, marker, color in [(0, "o", "#1D9E75"), (1, "s", "#D85A30")]:
+    for cls, marker, color in [(0, "o", "#D85A30"), (1, "s", "#1D9E75")]:
 
         pts = x[y == cls]
         ax.scatter(pts[:, 0], pts[:, 1], marker = marker, s = 120,
@@ -60,8 +66,8 @@ def example2d2class():
 
         ax.plot(x1val, x2plus, "k--", linewidth = 1, alpha = 0.5)
         ax.plot(x1val, x2minus, "k--", linewidth = 1, alpha = 0.5)
-        ax.set_xlabel("x1")
-        ax.set_ylabel("x2")
+        ax.set_xlabel("Humidity (%)")
+        ax.set_ylabel("Temperature (℃)")
         ax.set_title("linear SVM 2 classes")
         ax.legend()
         ax.grid(alpha = 0.3)
